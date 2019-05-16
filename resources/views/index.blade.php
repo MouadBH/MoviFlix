92995345a4e1be3ba9d8a66f18857c24d7956a5e
Mouad ZIANI <mouad.ziani1997@gmail.com>
2019-05-16 06:17:09 +0200

    Disable submit csrf token when sent form with GET method

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -53,7 +53,6 @@
                                     <div class="row justify-content-center">
                                         <div class="col-md-8 col-sm-10 col-12">
                                             <div class="form-group">
-                                                @csrf
                                                 <input name="search-keyword" type="text" id="search-keyword" value="" class="form-control" placeholder="Enter Movies or Series Title">
                                                 <button type="submit" class="btn btn-main btn-effect"><i class="fa fa-search"></i></button>
                                             </div>
